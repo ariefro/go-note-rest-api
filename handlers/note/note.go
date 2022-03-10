5fd68563e83d264687313f34f892847adb8ac454
Arief romadhon <ariefromadhon26@gmail.com>
2022-03-10 10:04:18 +0700

    add update note handler

--- handlers/note/note.go
+++ handlers/note/note.go
@@ -57,3 +57,46 @@ func CreateNote(c *fiber.Ctx) error {
 		"data": note,
 	})
 }
+
+func UpdateNote(c *fiber.Ctx) error {
+	type updateNote struct {
+		Title string `json:"title"`
+		SubTitle string `json:"subTitle"`
+		Text string `json:"text"`
+	}
+
+	db := database.DB
+	var note model.Note
+
+	id := c.Params("noteId")
+	db.Find(&note, "id = ?", id)
+	if note.ID == uuid.Nil {
+		return c.Status(404).JSON(fiber.Map{
+			"status": "error",
+			"message": "No notes present",
+			"data": nil,
+		})
+	}
+
+	var updateNoteData updateNote
+	err := c.BodyParser(&updateNoteData)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"status": "error",
+			"message": "Review your input",
+			"data": err,
+		})
+	}
+
+	note.Title = updateNoteData.Title
+	note.SubTitle = updateNoteData.SubTitle
+	note.Text = updateNoteData.Text
+
+	db.Save(&note)
+
+	return c.JSON(fiber.Map{
+		"status": "success",
+		"message": "Updated note",
+		"data": note,
+	})
+}
