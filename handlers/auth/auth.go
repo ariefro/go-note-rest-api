7cb50b8d5bea47823edf4b6f913a4fd4e41910ff
Arief romadhon <ariefromadhon26@gmail.com>
2022-03-12 11:11:58 +0700

    add handler get user by email

---
+++ handlers/auth/auth.go
@@ -0,0 +1,23 @@
+package auth
+
+import (
+	"errors"
+
+	"github.com/ariefro/notes-server/database"
+	"github.com/ariefro/notes-server/model"
+	"gorm.io/gorm"
+)
+
+func GetUserByEmail(e string) (*model.User, error) {
+	db := database.DB
+	var user model.User
+
+	err := db.Where(&model.User{Email: e}).Find(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
+}
