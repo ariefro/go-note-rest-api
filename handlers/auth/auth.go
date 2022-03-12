7e98cf2e3efd91e04adfaf9daa43bf417453143d
Arief romadhon <ariefromadhon26@gmail.com>
2022-03-12 11:27:17 +0700

    add handler get user by username

--- handlers/auth/auth.go
+++ handlers/auth/auth.go
@@ -21,3 +21,17 @@ func GetUserByEmail(e string) (*model.User, error) {
 	}
 	return &user, nil
 }
+
+func GetUserByUsername(u string) (*model.User, error) {
+	db := database.DB
+	var user model.User
+
+	err := db.Where(&model.User{Username: u}).Find(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
+}
